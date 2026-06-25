b40ccc7cbb13cce7b5d02541d27d5c96208c037d
Coolfan <coolfan1024@gmail.com>
2026-06-25 14:13:06 +0800

    fix(im): help link should say 配置 webhook not 获取 AccessToken
    IM 配置项是 WebHook URL + Secret，不存在 AccessToken。i18n key 改成
    已经存在的 ru-he-pei-zhi-webhook = "如何配置webhook"。

--- frontend/src/views/im/form.vue
+++ frontend/src/views/im/form.vue
@@ -36,7 +36,7 @@
 
       <template v-if="imDefList.length !== 0">
         <div class="im-form-help">
-          <a v-if="selectedImDef && selectedImDef.helpUrl" @click="jumpToHelp">{{ $t('ru-he-huo-qu-accesstoken') }}</a>
+          <a v-if="selectedImDef && selectedImDef.helpUrl" @click="jumpToHelp">{{ $t('ru-he-pei-zhi-webhook') }}</a>
           <span v-else></span>
           <div class="im-test-result">
             <span v-show="isCorrect !== 'init'" :class="isCorrect ? 'green-text' : 'error-text'">
