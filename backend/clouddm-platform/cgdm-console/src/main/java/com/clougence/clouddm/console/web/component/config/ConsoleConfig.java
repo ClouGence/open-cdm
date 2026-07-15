b0c15def11b57a2dbf540e7f1a9db894c547a2de
pika <759349196@qq.com>
2026-07-15 17:29:43 +0800

    fix(report): isolate installation report server

--- backend/clouddm-platform/cgdm-console/src/main/java/com/clougence/clouddm/console/web/component/config/ConsoleConfig.java
+++ backend/clouddm-platform/cgdm-console/src/main/java/com/clougence/clouddm/console/web/component/config/ConsoleConfig.java
@@ -48,6 +48,8 @@ public class ConsoleConfig {
 
     @Value("${clouddm.upgrade.server:server.cdmgr.com}")
     private String         upgradeServer;
+    @Value("${clouddm.installation.report.server:server.cdmgr.com}")
+    private String         installationReportServer;
 
     // for rsocket
     @Value("${clouddm.rsocket.dns:clouddm_console}")
