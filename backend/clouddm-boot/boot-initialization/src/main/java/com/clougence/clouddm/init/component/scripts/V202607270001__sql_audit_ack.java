9362402a36ac7cb73fdce1b547b180009120506c
zyc <zyc@hasor.net>
2026-08-03 11:01:38 +0800

    1. merge sql.

--- backend/clouddm-boot/boot-initialization/src/main/java/com/clougence/clouddm/init/component/scripts/V202607270001__sql_audit_ack.java
+++ backend/clouddm-boot/boot-initialization/src/main/java/com/clougence/clouddm/init/component/scripts/V202607270001__sql_audit_ack.java
@@ -15,11 +15,23 @@
  */
 package com.clougence.clouddm.init.component.scripts;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.clougence.clouddm.init.component.flyway.AbstractUpgradeJavaMigration;
+import com.clougence.utils.JsonUtils;
 
 public class V202607270001__sql_audit_ack extends AbstractUpgradeJavaMigration {
+
+    private static final String DM_OBJ    = "DM_OBJ";
+    private static final String DM_DDL    = "DM_DDL";
+    private static final String DM_DCL    = "DM_DCL";
+    private static final String DM_MANAGE = "DM_MANAGE";
 
     @Override
     public List<String> collectScript() {
@@ -65,5 +77,55 @@ public class V202607270001__sql_audit_ack extends AbstractUpgradeJavaMigration {
                         drop column constraints_json,
                         add column masking longtext null
                 """);
+    }
+
+    @Override
+    protected void afterMigrate(Connection connection) throws Exception {
+        try (PreparedStatement query = connection.prepareStatement("""
+                select id, res_auth_label
+                from dm_auth_res
+                where res_auth_label like '%"DM_OBJ"%'
+                   or res_auth_label like '%"DM_DCL"%'
+                """); ResultSet resultSet = query.executeQuery(); PreparedStatement update = connection.prepareStatement("""
+                update dm_auth_res
+                set res_auth_label = ?, gmt_modified = now()
+                where id = ?
+                """)) {
+            while (resultSet.next()) {
+                String oldLabels = resultSet.getString("res_auth_label");
+                String newLabels = migrateAuthLabels(oldLabels);
+                if (oldLabels.equals(newLabels)) {
+                    continue;
+                }
+
+                update.setString(1, newLabels);
+                update.setLong(2, resultSet.getLong("id"));
+                update.executeUpdate();
+            }
+        }
+    }
+
+    static String migrateAuthLabels(String authLabels) {
+        List<String> oldLabels = JsonUtils.toListUseType(authLabels, String.class);
+        List<String> newLabels = new ArrayList<>(oldLabels.size());
+        Set<String> migratedTargets = new HashSet<>();
+        boolean changed = false;
+
+        for (String oldLabel : oldLabels) {
+            String newLabel = switch (oldLabel) {
+                case DM_OBJ -> DM_DDL;
+                case DM_DCL -> DM_MANAGE;
+                default -> oldLabel;
+            };
+            changed |= !newLabel.equals(oldLabel);
+
+            if ((DM_DDL.equals(newLabel) || DM_MANAGE.equals(newLabel)) && !migratedTargets.add(newLabel)) {
+                changed = true;
+                continue;
+            }
+            newLabels.add(newLabel);
+        }
+
+        return changed ? JsonUtils.toJson(newLabels) : authLabels;
     }
 }
